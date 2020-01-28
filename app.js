c6682f1c6c695a573cd8261d1da576c4b33c8050
Earon Pabon <pabs@migo.tv>
2020-01-28 08:26:04 +0800

    removing non-needed api

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ db.connectToDB(CONSTANT.MONGO,mongoose)
 })
 
 //middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 //app.use(authChecker.validate);
 app.use(cors());
 app.use('/graphql',grapqlHTTP({
@@ -55,8 +55,4 @@ app.listen(process.env.PORT || 1973,()=>{
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname + '/views/index.html'));
-});
-
-app.post('/api/v1.0.0/image',(req,res)=>{
-    imageUpdater.saveImage(req,res,Activity);
 });
